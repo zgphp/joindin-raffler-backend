b3d196fd276164a549ef0ee8d4f045f0668b1cc0
Miro Svrtan <miro@mirosvrtan.me>
2017-11-02 16:41:51 +0100

    Add API for exposing joind in event actions

---
+++ features/bootstrap/ApiContext.php
@@ -0,0 +1,72 @@
+<?php
+
+use App\Repository\JoindInEventRepository;
+use Behat\Behat\Context\Context;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Webmozart\Assert\Assert;
+
+/**
+ * This context class contains the definitions of the steps used by the demo
+ * feature file. Learn how to get started with Behat and BDD on Behat's website.
+ *
+ * @see http://behat.org/en/latest/quick_start.html
+ */
+class ApiContext implements Context
+{
+    /**
+     * @var KernelInterface
+     */
+    private $kernel;
+
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @Given there are no meetups in the system
+     */
+    public function thereAreNoMeetupsInTheSystem()
+    {
+        foreach ($this->getEventRepository()->findAll() as $joindInEvent) {
+            $this->getEntityManager()->remove($joindInEvent);
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * @When I fetch meetup data from Joind.in
+     */
+    public function iFetchMeetupDataFromJoindIn()
+    {
+        $this->getGuzzle()->get('http://localhost:8000/joindin/fetch');
+    }
+
+    /**
+     * @Then there should be :count ZgPHP meetups in system
+     */
+    public function thereShouldBeZgphpMeetupsInSystem(int $count)
+    {
+        $response = $this->getGuzzle()->get('http://localhost:8000/joindin/list');
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        Assert::count($data, $count);
+    }
+
+    private function getGuzzle(): \GuzzleHttp\Client
+    {
+        return $this->kernel->getContainer()->get(\GuzzleHttp\Client::class);
+    }
+
+    private function getEntityManager(): EntityManager
+    {
+        return $this->kernel->getContainer()->get('doctrine.orm.entity_manager');
+    }
+
+    private function getEventRepository(): JoindInEventRepository
+    {
+        return $this->kernel->getContainer()->get(JoindInEventRepository::class);
+    }
+}
---
+++ src/Controller/JoindInEventController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\JoindInEventRepository;
+use App\Service\JoindInClient;
+use App\Service\JoindInEventRetrieval;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class JoindInEventController
+{
+    /**
+     * @var JoindInEventRepository
+     */
+    private $eventRepository;
+    /**
+     * @var JoindInClient
+     */
+    private $joindInClient;
+
+    /**
+     * @var JoindInEventRetrieval
+     */
+    private $joindInEventRetrieval;
+
+    public function __construct(
+        JoindInEventRetrieval $joindInEventRetrieval,
+        JoindInEventRepository $eventRepository,
+        JoindInClient $joindInClient
+    ) {
+        $this->eventRepository       = $eventRepository;
+        $this->joindInClient         = $joindInClient;
+        $this->joindInEventRetrieval = $joindInEventRetrieval;
+    }
+
+    public function fetch()
+    {
+        $this->joindInEventRetrieval->fetch();
+
+        return new JsonResponse('OK');
+    }
+
+    public function eventList()
+    {
+        $events = $this->eventRepository->findAll();
+
+        return new JsonResponse($events);
+    }
+}
