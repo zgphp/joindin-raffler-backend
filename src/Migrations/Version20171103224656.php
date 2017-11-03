c441967bfa91241d9b380674b8db37eab641ebae
Miro Svrtan <miro@mirosvrtan.me>
2017-11-03 23:38:13 +0100

    Add JoindInTalk entity & repository #55
    In order to have a list of eligible JoindIn commenters to raffle
    For organizers ,
    We will store talks from ZgPHP events

---
+++ src/Migrations/Version20171103224656.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171103224656 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE joindinTalks (id INT NOT NULL, event_id INT DEFAULT NULL, title VARCHAR(200) NOT NULL, importedAt TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_FB7FC5EA71F7E88B ON joindinTalks (event_id)');
+        $this->addSql('ALTER TABLE joindinTalks ADD CONSTRAINT FK_FB7FC5EA71F7E88B FOREIGN KEY (event_id) REFERENCES joindinEvents (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE joindinTalks');
+    }
+}
