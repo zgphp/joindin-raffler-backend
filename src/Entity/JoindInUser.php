ef04c47bd875b2dca8ea8dfd63597360c3a4f359
Miro Svrtan <miro@mirosvrtan.me>
2017-11-04 13:26:46 +0100

    Add JoindInUser entity & repo

---
+++ src/Entity/JoindInUser.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\JoindInUserRepository")
+ * @ORM\Table(name="joindinUsers")
+ */
+class JoindInUser implements \JsonSerializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     *
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     *
+     * @var string
+     */
+    private $displayName;
+
+    public function __construct(int $id, string $username, string $displayName)
+    {
+        $this->id          = $id;
+        $this->username    = $username;
+        $this->displayName = $displayName;
+    }
+
+    public function setUsername(string $username)
+    {
+        $this->username = $username;
+    }
+
+    public function setDisplayName(string $displayName)
+    {
+        $this->displayName = $displayName;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getDisplayName(): string
+    {
+        return $this->displayName;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id'          => $this->id,
+            'username'    => $this->username,
+            'displayName' => $this->displayName,
+        ];
+    }
+}
---
+++ src/Migrations/Version20171104122657.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171104122657 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE joindinUsers (id INT NOT NULL, username VARCHAR(40) NOT NULL, displayName VARCHAR(200) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE joindinUsers');
+    }
+}
