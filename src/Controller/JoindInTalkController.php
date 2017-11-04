1c0da3ff8987709242cba44034aadafd2e7b01de
Miro Svrtan <miro@mirosvrtan.me>
2017-11-04 12:09:29 +0100

    Expose talks via API #55

---
+++ src/Controller/JoindInTalkController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\JoindInEventRepository;
+use App\Repository\JoindInTalkRepository;
+use App\Service\JoindInTalkRetrieval;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class JoindInTalkController
+{
+    /** @var JoindInTalkRetrieval */
+    private $joindInTalkRetrieval;
+    /** @var JoindInEventRepository */
+    private $eventRepository;
+    /** @var JoindInTalkRepository */
+    private $talkRepository;
+
+    public function __construct(
+        JoindInTalkRetrieval $joindInTalkRetrieval,
+        JoindInEventRepository $eventRepository,
+        JoindInTalkRepository $talkRepository
+    ) {
+        $this->joindInTalkRetrieval = $joindInTalkRetrieval;
+        $this->eventRepository      = $eventRepository;
+        $this->talkRepository       = $talkRepository;
+    }
+
+    public function fetch()
+    {
+        foreach ($this->eventRepository->findAll() as $event) {
+            $this->joindInTalkRetrieval->fetch($event);
+        }
+
+        return new JsonResponse('OK');
+    }
+
+    public function talkList()
+    {
+        $talks = $this->talkRepository->findAll();
+
+        return new JsonResponse($talks);
+    }
+}
