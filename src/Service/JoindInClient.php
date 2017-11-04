a9fb993bf5d4dcfb566caa32e6fd76ee8f8d5b84
Miro Svrtan <miro@mirosvrtan.me>
2017-11-04 13:15:09 +0100

    Support fetching comments for a talk in JoindInClient #64

--- src/Service/JoindInClient.php
+++ src/Service/JoindInClient.php
@@ -3,6 +3,9 @@
 namespace App\Service;
 
 use App\Entity\JoindInEvent;
+use App\Entity\JoindInTalk;
+use App\JoindIn\CommentData;
+use App\JoindIn\CommentDataFactory;
 use App\JoindIn\EventData;
 use App\JoindIn\EventDataFactory;
 use App\JoindIn\TalkData;
@@ -17,14 +20,21 @@ class JoindInClient
     private $eventDataFactory;
     /** @var TalkDataFactory */
     private $talkDataFactory;
+    /** @var CommentDataFactory */
+    private $commentDataFactory;
 
     private $baseUrl = 'https://api.joind.in/v2.1';
 
-    public function __construct(Client $client, EventDataFactory $eventDataFactory, TalkDataFactory $talkDataFactory)
-    {
-        $this->client           = $client;
-        $this->eventDataFactory = $eventDataFactory;
-        $this->talkDataFactory  = $talkDataFactory;
+    public function __construct(
+        Client $client,
+        EventDataFactory $eventDataFactory,
+        TalkDataFactory $talkDataFactory,
+        CommentDataFactory $commentDataFactory
+    ) {
+        $this->client             = $client;
+        $this->eventDataFactory   = $eventDataFactory;
+        $this->talkDataFactory    = $talkDataFactory;
+        $this->commentDataFactory = $commentDataFactory;
     }
 
     /** @return EventData[] */
@@ -58,6 +68,24 @@ class JoindInClient
 
         foreach ($raw['talks'] as $item) {
             $results[] = $this->talkDataFactory->create($item, $event);
+        }
+
+        return $results;
+    }
+
+    /** @return CommentData[] */
+    public function fetchCommentsForTalk(JoindInTalk $talk): array
+    {
+        $url = $this->baseUrl.'/talks/'.$talk->getId().'/comments';
+
+        $response = $this->client->get($url);
+
+        $raw = json_decode($response->getBody()->getContents(), true);
+
+        $results = [];
+
+        foreach ($raw['comments'] as $item) {
+            $results[] = $this->commentDataFactory->create($item, $talk);
         }
 
         return $results;
