c66716c95453f55b580f947e86564e971dfceb4e
Miro Svrtan <miro@mirosvrtan.me>
2017-11-04 12:10:27 +0100

    JoindInClient should retrieve talks for a meetup #55

--- src/Service/JoindInClient.php
+++ src/Service/JoindInClient.php
@@ -2,7 +2,11 @@
 
 namespace App\Service;
 
+use App\Entity\JoindInEvent;
+use App\JoindIn\EventData;
 use App\JoindIn\EventDataFactory;
+use App\JoindIn\TalkData;
+use App\JoindIn\TalkDataFactory;
 use GuzzleHttp\Client;
 
 class JoindInClient
@@ -11,15 +15,19 @@ class JoindInClient
     private $client;
     /** @var EventDataFactory */
     private $eventDataFactory;
+    /** @var TalkDataFactory */
+    private $talkDataFactory;
 
     private $baseUrl = 'https://api.joind.in/v2.1';
 
-    public function __construct(Client $client, EventDataFactory $eventDataFactory)
+    public function __construct(Client $client, EventDataFactory $eventDataFactory, TalkDataFactory $talkDataFactory)
     {
         $this->client           = $client;
         $this->eventDataFactory = $eventDataFactory;
+        $this->talkDataFactory  = $talkDataFactory;
     }
 
+    /** @return EventData[] */
     public function fetchZgPhpEvents(): array
     {
         $url = $this->baseUrl.'/events?title=zgphp&resultsperpage=30';
@@ -32,6 +40,24 @@ class JoindInClient
 
         foreach ($raw['events'] as $item) {
             $results[] = $this->eventDataFactory->create($item);
+        }
+
+        return $results;
+    }
+
+    /** @return TalkData[] */
+    public function fetchTalksForEvent(JoindInEvent $event): array
+    {
+        $url = $this->baseUrl.'/events/'.$event->getId().'/talks';
+
+        $response = $this->client->get($url);
+
+        $raw = json_decode($response->getBody()->getContents(), true);
+
+        $results = [];
+
+        foreach ($raw['talks'] as $item) {
+            $results[] = $this->talkDataFactory->create($item, $event);
         }
 
         return $results;
