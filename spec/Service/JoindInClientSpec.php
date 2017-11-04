83dda43608a0fd196e5534cef1f29bdf86e9cdbe
Miro Svrtan <miro@mirosvrtan.me>
2017-11-04 13:15:09 +0100

    Support fetching comments for a talk in JoindInClient #64

--- spec/Service/JoindInClientSpec.php
+++ spec/Service/JoindInClientSpec.php
@@ -3,6 +3,9 @@
 namespace spec\App\Service;
 
 use App\Entity\JoindInEvent;
+use App\Entity\JoindInTalk;
+use App\JoindIn\CommentData;
+use App\JoindIn\CommentDataFactory;
 use App\JoindIn\EventData;
 use App\JoindIn\EventDataFactory;
 use App\JoindIn\TalkData;
@@ -16,9 +19,9 @@ use Psr\Http\Message\StreamInterface;
 
 class JoindInClientSpec extends ObjectBehavior
 {
-    public function let(Client $client, EventDataFactory $eventDataFactory, TalkDataFactory $talkDataFactory)
+    public function let(Client $client, EventDataFactory $eventDataFactory, TalkDataFactory $talkDataFactory, CommentDataFactory $commentDataFactory)
     {
-        $this->beConstructedWith($client, $eventDataFactory, $talkDataFactory);
+        $this->beConstructedWith($client, $eventDataFactory, $talkDataFactory, $commentDataFactory);
     }
 
     public function it_is_initializable()
@@ -143,5 +146,52 @@ class JoindInClientSpec extends ObjectBehavior
 
         $this->fetchTalksForEvent($event)
             ->shouldReturn([$talkData]);
+    }
+
+    public function it_will_return_comments_for_given_talk(
+        Client $client,
+        CommentDataFactory $commentDataFactory,
+        Response $response,
+        StreamInterface $body,
+        JoindInTalk $talk,
+        CommentData $commentData
+    ) {
+        $talk->getId()
+            ->shouldBeCalled()
+            ->willReturn(2345);
+
+        $client->get('https://api.joind.in/v2.1/talks/2345/comments')
+            ->shouldBeCalled()
+            ->willReturn($response);
+
+        $response->getBody()
+            ->shouldBeCalled()
+            ->willReturn($body);
+
+        $content = '
+{
+  "comments": [
+    {
+      "rating": 5,
+      "comment": "Great talk",
+      "user_display_name": "User Name",
+      "username": "user_name",
+      "uri": "https://api.joind.in/v2.1/talk_comments/34567",
+      "user_uri": "https://api.joind.in/v2.1/users/12"
+    }
+  ]
+}
+        ';
+
+        $body->getContents()
+            ->shouldBeCalled()
+            ->willReturn($content);
+
+        $commentDataFactory->create(Argument::any(), $talk)
+            ->shouldBeCalled()
+            ->willReturn($commentData);
+
+        $this->fetchCommentsForTalk($talk)
+            ->shouldReturn([$commentData]);
     }
 }
