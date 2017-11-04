3587919913f44b99f4df728c55f9a0443b4dabed
Miro Svrtan <miro@mirosvrtan.me>
2017-11-04 12:09:29 +0100

    Expose talks via API #55

--- features/bootstrap/ApiContext.php
+++ features/bootstrap/ApiContext.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Entity\JoindInEvent;
 use App\Repository\JoindInEventRepository;
 use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Webmozart\Assert\Assert;
@@ -30,9 +32,28 @@ class ApiContext implements Context
     public function thereAreNoMeetupsInTheSystem()
     {
         foreach ($this->getEventRepository()->findAll() as $joindInEvent) {
+            foreach ($joindInEvent->getTalks() as $joindInTalk) {
+                $this->getEntityManager()->remove($joindInTalk);
+            }
+
             $this->getEntityManager()->remove($joindInEvent);
-            $this->getEntityManager()->flush();
         }
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @Given we have this meetups in the system
+     */
+    public function weHaveThisMeetupsInTheSystem(TableNode $table)
+    {
+        $this->thereAreNoMeetupsInTheSystem();
+
+        foreach ($table as $row) {
+            $event = new JoindInEvent((int) $row['id'], $row['title'], new DateTime($row['startDate']), new DateTime($row['endDate']));
+
+            $this->getEntityManager()->persist($event);
+        }
+        $this->getEntityManager()->flush();
     }
 
     /**
@@ -44,11 +65,31 @@ class ApiContext implements Context
     }
 
     /**
+     * @When I fetch meetup talks from Joind.in
+     */
+    public function iFetchMeetupTalksFromJoindIn()
+    {
+        $this->getGuzzle()->get('http://test.raffler.loc:8000/joindin/talks/fetch');
+    }
+
+    /**
      * @Then there should be :count ZgPHP meetups in system
      */
     public function thereShouldBeZgphpMeetupsInSystem(int $count)
     {
         $response = $this->getGuzzle()->get('http://test.raffler.loc:8000/joindin/list');
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        Assert::count($data, $count);
+    }
+
+    /**
+     * @Then there should be :count talks in system
+     */
+    public function thereShouldBeTalksInSystem(int $count)
+    {
+        $response = $this->getGuzzle()->get('http://test.raffler.loc:8000/joindin/talks/');
 
         $data = json_decode($response->getBody()->getContents(), true);
 
