360558c0d05403d4cd79501399c920d6dc83f49b
Miro Svrtan <miro@mirosvrtan.me>
2017-11-04 13:35:10 +0100

    Add JoindInComment entity & repo

---
+++ src/Entity/JoindInComment.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\JoindInCommentRepository")
+ * @ORM\Table(name="joindinComments")
+ */
+class JoindInComment implements \JsonSerializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     *
+     * @var string
+     */
+    private $comment;
+
+    /**
+     * @ORM\Column(type="integer")
+     *
+     * @var int
+     */
+    private $rating;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\JoindInUser", inversedBy="comments")
+     *
+     * @var JoindInUser
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\JoindInTalk", inversedBy="comments")
+     *
+     * @var JoindInTalk
+     */
+    private $talk;
+
+    public function __construct(int $id, string $comment, int $rating, JoindInUser $user, JoindInTalk $talk)
+    {
+        $this->id      = $id;
+        $this->comment = $comment;
+        $this->rating  = $rating;
+        $this->user    = $user;
+        $this->talk    = $talk;
+    }
+
+    public function setComment(string $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function setRating(int $rating)
+    {
+        $this->rating = $rating;
+    }
+
+    public function setUser(JoindInUser $user)
+    {
+        $this->user = $user;
+    }
+
+    public function setTalk(JoindInTalk $talk)
+    {
+        $this->talk = $talk;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+
+    public function getRating(): int
+    {
+        return $this->rating;
+    }
+
+    public function getUser(): JoindInUser
+    {
+        return $this->user;
+    }
+
+    public function getTalk(): JoindInTalk
+    {
+        return $this->talk;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id'      => $this->id,
+            'comment' => $this->comment,
+            'rating'  => $this->rating,
+            'user'    => $this->user->jsonSerialize(),
+            'talk'    => $this->talk->jsonSerialize(),
+        ];
+    }
+}
---
+++ src/Migrations/Version20171104123424.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171104123424 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE joindinComments (id INT NOT NULL, user_id INT DEFAULT NULL, talk_id INT DEFAULT NULL, comment TEXT NOT NULL, rating INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_FA3AD44EA76ED395 ON joindinComments (user_id)');
+        $this->addSql('CREATE INDEX IDX_FA3AD44E6F0601D5 ON joindinComments (talk_id)');
+        $this->addSql('ALTER TABLE joindinComments ADD CONSTRAINT FK_FA3AD44EA76ED395 FOREIGN KEY (user_id) REFERENCES joindinUsers (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE joindinComments ADD CONSTRAINT FK_FA3AD44E6F0601D5 FOREIGN KEY (talk_id) REFERENCES joindinTalks (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE joindinComments');
+    }
+}
