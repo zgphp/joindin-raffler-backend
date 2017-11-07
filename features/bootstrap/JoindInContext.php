dc161f3bb5193487d54ad924ab90976eb8082d69
Miro Svrtan <miro@mirosvrtan.me>
2017-11-07 16:20:33 +0100

    Introduce JoindInContext #94

---
+++ features/bootstrap/JoindInContext.php
@@ -0,0 +1,83 @@
+<?php
+
+use App\Service\JoindInCommentRetrieval;
+use App\Service\JoindInEventRetrieval;
+use App\Service\JoindInTalkRetrieval;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Webmozart\Assert\Assert;
+
+class JoindInContext extends BaseContext
+{
+    use FixturesTrait;
+
+    /** @var KernelInterface */
+    private $kernel;
+
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @When I fetch meetup data from Joind.in
+     */
+    public function iFetchMeetupDataFromJoindIn()
+    {
+        $service = $this->getService(JoindInEventRetrieval::class);
+
+        $service->fetch();
+    }
+
+    /**
+     * @When I fetch meetup talks from Joind.in
+     */
+    public function iFetchMeetupTalksFromJoindIn()
+    {
+        $service = $this->getService(JoindInTalkRetrieval::class);
+
+        foreach ($this->getEventRepository()->findAll() as $event) {
+            $service->fetch($event);
+        }
+    }
+
+    /**
+     * @When I fetch meetup talk comments from Joind.in
+     */
+    public function iFetchMeetupTalkCommentsFromJoindIn()
+    {
+        $service = $this->getService(JoindInCommentRetrieval::class);
+
+        foreach ($this->getTalkRepository()->findAll() as $talk) {
+            $service->fetch($talk);
+        }
+    }
+
+    /**
+     * @Then there should be :count ZgPHP meetups in system
+     */
+    public function thereShouldBeZgphpMeetupsInSystem(int $count)
+    {
+        Assert::count($this->getEventRepository()->findAll(), $count);
+    }
+
+    /**
+     * @Then there should be :count talks in system
+     */
+    public function thereShouldBeTalksInSystem(int $count)
+    {
+        Assert::count($this->getTalkRepository()->findAll(), $count);
+    }
+
+    /**
+     * @Then there should be :count comment in system
+     */
+    public function thereShouldBeCommentInSystem(int $count)
+    {
+        Assert::count($this->getCommentRepository()->findAll(), $count);
+    }
+
+    protected function getService(string $name)
+    {
+        return $this->kernel->getContainer()->get($name);
+    }
+}
