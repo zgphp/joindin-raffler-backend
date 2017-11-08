cf80365b505a0d4610e740edfafb38ba59060a5b
Miro Svrtan <miro@mirosvrtan.me>
2017-11-08 15:46:11 +0100

    Implement our own UUID mapping
    Orginal one returns a UUID object when loading data from database while we want to get a string

---
+++ src/UuidType.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * This file is part of the ramsey/uuid-doctrine library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Ben Ramsey <http://benramsey.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @see https://packagist.org/packages/ramsey/uuid-doctrine Packagist
+ * @see https://github.com/ramsey/uuid-doctrine GitHub
+ */
+
+namespace App;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * Field type mapping for the Doctrine Database Abstraction Layer (DBAL).
+ *
+ * UUID fields will be stored as a string in the database and converted back to
+ * the Uuid value object when querying.
+ */
+class UuidType extends Type
+{
+    /**
+     * @var string
+     */
+    const NAME = 'uuid';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param array                                     $fieldDeclaration
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+     */
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return $platform->getGuidTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param string|null                               $value
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+     */
+    public function convertToPhpValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param UuidInterface|null                        $value
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof Uuid || Uuid::isValid($value)) {
+            return (string) $value;
+        }
+
+        throw ConversionException::conversionFailed($value, static::NAME);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return static::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+     *
+     * @return bool
+     */
+    public function requiresSqlCommentHint(AbstractPlatform $platform)
+    {
+        return true;
+    }
+}
