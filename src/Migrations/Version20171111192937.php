5a893af35b6e5b96fdc4fe343e11ceabf753ee3b
Tomo Šala <hcs.omot@gmail.com>
2017-11-12 14:23:42 +0100

    Remove schema recreation in migration files
    Some migration files sliped through with schema recreation commands
    still in. This will clean them up.
    
    Doctrine always adds this command, so until we learn how to disable
    it, we'll need to manually remove it from down() migrations.

--- src/Migrations/Version20171111192937.php
+++ src/Migrations/Version20171111192937.php
@@ -26,10 +26,9 @@ class Version20171111192937 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        // this down() migration is modified as to not try to recreate an existing 'public' schema
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE joindinEvents ADD enddate TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
     }
 }
