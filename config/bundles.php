3466439ef9051ac635b44e0cc4d3131c453203d8
Miro Svrtan <miro@mirosvrtan.me>
2017-11-28 13:26:45 +0100

    Configuring friendsofsymfony/user-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
