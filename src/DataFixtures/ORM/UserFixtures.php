e7667168393e57224721293f311666fe063c6db1
Miro Svrtan <miro@mirosvrtan.me>
2017-12-19 17:57:52 +0100

    Task #161: Add UserFixtures
    In order to easily test on dev
    For maintainers
    We need to add an admin user to fixtures

---
+++ src/DataFixtures/ORM/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures\ORM;
+
+use App\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixtures extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setEmail('info+raffler@zgphp.org');
+        $admin->setEnabled(true);
+        $admin->setPlainPassword('admin');
+        $admin->setRoles(['ROLE_SUPER_ADMIN']);
+
+        $manager->persist($admin);
+        $this->addReference('user-admin', $admin);
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 10;
+    }
+}
